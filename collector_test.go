106ae4d1970dc5642af7b250b5ec3ce101edc587
Rudy Rigot <rudy.rigot@gmail.com>
2016-04-08 15:41:38 -0700

    assertArrayEqual -> assertIntArrayEqual + message mandatory

--- collector_test.go
+++ collector_test.go
@@ -3,5 +3,6 @@ package collector
 import "testing"
 
 func TestC(t *testing.T) {
-	assertEqualM(t, len([]int{0, 1, 2}), len(C([]int{0, 1, 2})), "C function should return array of same length")
+	assertEqual(t, len([]int{0, 1, 2}), len(C([]int{0, 1, 2})), "C function should return array of same length")
+	assertIntArrayEqual(t, []int{0, 1, 2}, C([]int{0, 1, 2}), "C function should return same array")
 }
--- tests.go
+++ tests.go
@@ -2,14 +2,21 @@ package collector
 
 import "testing"
 
-func assertEqual(t *testing.T, in interface{}, want interface{}) {
+func assertEqual(t *testing.T, in interface{}, want interface{}, message string) {
 	if in != want {
-		t.Errorf("[%#v != %#v]", in, want)
+		t.Errorf("[%#v != %#v] %s", in, want, message)
 	}
 }
 
-func assertEqualM(t *testing.T, in interface{}, want interface{}, message string) {
-	if in != want {
+func assertIntArrayEqual(t *testing.T, in []int, want []int, message string) {
+	if len(in) != len(want) {
 		t.Errorf("[%#v != %#v] %s", in, want, message)
+	} else {
+		for i, elem := range in {
+			if want[i] != elem {
+				t.Errorf("[%#v != %#v] %s", in, want, message)
+				return
+			}
+		}
 	}
 }
